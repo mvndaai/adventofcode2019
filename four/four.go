78d7112d89d88208e590c408d192f0902023a6d2
Jason Mavandi <jmavandi@solutionreach.com>
2019-12-05 11:35:04 -0700

    day 4 part 2

--- four/four.go
+++ four/four.go
@@ -43,6 +43,43 @@ func validNumber(n int) bool {
 	return true
 }
 
+func validNumber2(n int) bool {
+	s := intToSlice(n)
+
+	sideByside := false
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] > s[i+1] {
+			return false
+		}
+
+		if s[i] == s[i+1] {
+			t := true
+			if i-1 >= 0 {
+				if s[i-1] == s[i] {
+					t = false
+				}
+			} else {
+				log.Println("a", i, n)
+			}
+			if i+2 < len(s) {
+				if s[i+2] == s[i] {
+					t = false
+				}
+			}
+
+			if t {
+				sideByside = t
+			}
+		}
+	}
+
+	if !sideByside {
+		return false
+	}
+
+	return true
+}
+
 func main() {
 	input := "137683-596253"
 	i := strings.Split(input, "-")
@@ -58,9 +95,11 @@ func main() {
 
 	count := 0
 	for i := start; i <= end; i++ {
-		if validNumber(i) {
+		if validNumber2(i) {
 			count++
 		}
 	}
 	fmt.Println(count)
+	// Failed with 1065 (too low)
+	// Failed with 1372 (too high)
 }
--- four/four_test.go
+++ four/four_test.go
@@ -23,3 +23,31 @@ func Test(t *testing.T) {
 		})
 	}
 }
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		in  int
+		out bool
+	}{
+		{in: 112233, out: true},
+		{in: 123444, out: false},
+		{in: 111122, out: true},
+
+		{in: 111111, out: false},
+		{in: 223450, out: false},
+		{in: 123789, out: false},
+
+		{in: 137683, out: false},
+		{in: 596253, out: false},
+
+		{in: 577999, out: true},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.in), func(t *testing.T) {
+			if validNumber2(tt.in) != tt.out {
+				t.Error("did not match")
+			}
+		})
+	}
+}
